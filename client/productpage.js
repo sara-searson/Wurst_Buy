967cde523c3ad80b70c953258e448c2851fbe7d9
sara-searson <sara.r.searson@gmail.com>
2024-10-17 11:42:04 -0400

    Added filtering by brand to product page

--- client/productpage.js
+++ client/productpage.js
@@ -2,7 +2,7 @@ const searchButton = document.getElementById("goButton")
 const about = document.getElementsByClassName("about")
 const hash = window.location.hash.substring(1); // Remove the '#' character
 const params = new URLSearchParams(hash);
-const brand = params.get('brand');
+let brand = params.get('brand');
 console.log(brand)
 
 function goButton(){
@@ -11,14 +11,20 @@ function goButton(){
 
 searchButton.addEventListener("click", goButton)
 
+const firstContainer = document.querySelector('.firstContainer')
+const secondContainer = document.querySelector('.secondContainer')
+const thirdContainer = document.querySelector('.thirdContainer')
+const fourthContainer = document.querySelector('.fourthContainer')
+const logos = document.querySelectorAll('.logos')
+
 const product1 = document.querySelector('#product1')
-const productContainer1 = document.querySelector('#productContainer1')
+const productContainer1 = document.querySelector('#prod1Text')
 const product2 = document.querySelector('#product2')
-const productContainer2 = document.querySelector('#productContainer2')
+const productContainer2 = document.querySelector('#prod2Text')
 const product3 = document.querySelector('#product3')
-const productContainer3 = document.querySelector('#productContainer3')
+const productContainer3 = document.querySelector('#prod3Text')
 const product4 = document.querySelector('#product4')
-const productContainer4 = document.querySelector('#productContainer4')
+const productContainer4 = document.querySelector('#prod4Text')
 
 
 //search bar functionality
@@ -37,7 +43,35 @@ resultsList.innerHTML = ''
 filteredItems.forEach(item => resultsList.appendChild(item))
 })
 
-const getInfo = async (num, position, imgPosition) => {
+const getImage = async (brand, position) => {
+    let response = await axios.get(`http://localhost:3001/brands/name/${brand}`);
+    console.log(response)
+    let image = response.data.logo_url;
+    console.log(image)
+    console.log(position)
+    position.setAttribute ("src", image);
+    console.log(response.data.brand_name)
+    position.setAttribute('id', response.data.brand_name)
+};
+
+
+const populateBrands = () => {
+    getImage('Dell', firstContainer);
+    getImage('Apple', secondContainer);
+    getImage('HP', thirdContainer);
+    getImage('Lenovo', fourthContainer);
+};
+
+populateBrands ()
+
+// const resetInfo = (position) => {
+//     while (position.firstChild) {
+//         position.removeChild(position.firstChild)
+//     }
+// }
+
+const getInfo = async (brand, num, position, imgPosition) => {
+    // resetInfo(position)
     let response = await axios.get(`http://localhost:3001/products/brand/${brand}`);
     console.log(response)
     let computer = response.data[num]
@@ -46,39 +80,40 @@ const getInfo = async (num, position, imgPosition) => {
     console.log(imgPosition)
     imgPosition.setAttribute ("src", image);
 
-    if (computer.is_laptop === true) {
-        let desc = position.appendChild(document.createElement("p"))  
-        desc.textContent = `Laptop`
-        desc.classList.add("computerText")
-    } else {
-        let desc = position.appendChild(document.createElement("p"))  
-        desc.textContent = `Desktop`
-        desc.classList.add("computerText")
-    }
+    let desc = position.children[0]  
+    computer.is_laptop === true ? desc.textContent = 'Laptop' : desc.textContent = 'Desktop'
 
-    let storage = position.appendChild(document.createElement("p"))  
-    storage.textContent = `Storage: ${computer.storage_GB}GB`
-    storage.classList.add("computerText")
+    let storage = position.children[1]
+    storage.textContent = `Storage: ${computer.storage_GB}`
 
-    let ram = position.appendChild(document.createElement("p"))  
+    let ram = position.children[2]
     ram.textContent = `RAM: ${computer.ram_GB}GB`
-    ram.classList.add("computerText")
 
-    let cpu = position.appendChild(document.createElement("p"))  
+    let cpu = position.children[3]
     cpu.textContent = `CPU: ${computer.cpu}`
-    cpu.classList.add("computerText")
 
-    let gpu = position.appendChild(document.createElement("p"))  
+    let gpu = position.children[4]
     gpu.textContent = `Graphics: ${computer.gpu}`
-    gpu.classList.add("computerText")
+
+    let price = position.children[5]
+    price.textContent = `$${computer.price}`
   };
   
   
-  const populatePage = () => {
-    getInfo('0', productContainer1, product1);
-    getInfo('1', productContainer2, product2);
-    getInfo('2', productContainer3, product3);
-    getInfo('3', productContainer4, product4);
+  const populatePage = (brand) => {
+    getInfo(brand, '0', productContainer1, product1);
+    getInfo(brand, '1', productContainer2, product2);
+    getInfo(brand, '2', productContainer3, product3);
+    getInfo(brand, '3', productContainer4, product4);
   };
   
-  populatePage ()
+  populatePage (brand)
+
+  logos.forEach((func) => {
+    func.addEventListener('click', () => {
+      console.log('clicked')
+        const brandName = func.getAttribute('id').toLocaleLowerCase()
+        window.location.href = `/client/productpage.html#brand=${encodeURIComponent(brandName)}`//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/encodeURIComponent
+        populatePage(brandName)
+    })
+  })
