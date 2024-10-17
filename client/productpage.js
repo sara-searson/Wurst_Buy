619ecc1b5dd7597f1e296b6fbb98b36b6fc16768
sara-searson <sara.r.searson@gmail.com>
2024-10-16 22:59:26 -0400

    Added product page load info and review schema, along with first review!

--- client/productpage.js
+++ client/productpage.js
@@ -8,6 +8,15 @@ function goButton(){
 
 searchButton.addEventListener("click", goButton)
 
+const product1 = document.querySelector('#product1')
+const productContainer1 = document.querySelector('#productContainer1')
+const product2 = document.querySelector('#product2')
+const productContainer2 = document.querySelector('#productContainer2')
+const product3 = document.querySelector('#product3')
+const productContainer3 = document.querySelector('#productContainer3')
+const product4 = document.querySelector('#product4')
+const productContainer4 = document.querySelector('#productContainer4')
+
 
 //search bar functionality
 
@@ -24,3 +33,49 @@ const filteredItems = Array.from(resultsList.children).filter(item => {
 resultsList.innerHTML = ''
 filteredItems.forEach(item => resultsList.appendChild(item))
 })
+
+const getInfo = async (num, position, imgPosition) => {
+    let response = await axios.get(`http://localhost:3001/products/brand/apple`);
+    console.log(response)
+    let computer = response.data[num]
+    let image = computer.product_image;
+    console.log(image)
+    console.log(imgPosition)
+    imgPosition.setAttribute ("src", image);
+
+    if (computer.is_laptop === true) {
+        let desc = position.appendChild(document.createElement("p"))  
+        desc.textContent = `Laptop`
+        desc.classList.add("computerText")
+    } else {
+        let desc = position.appendChild(document.createElement("p"))  
+        desc.textContent = `Desktop`
+        desc.classList.add("computerText")
+    }
+
+    let storage = position.appendChild(document.createElement("p"))  
+    storage.textContent = `Storage: ${computer.storage_GB}GB`
+    storage.classList.add("computerText")
+
+    let ram = position.appendChild(document.createElement("p"))  
+    ram.textContent = `RAM: ${computer.ram_GB}GB`
+    ram.classList.add("computerText")
+
+    let cpu = position.appendChild(document.createElement("p"))  
+    cpu.textContent = `CPU: ${computer.cpu}`
+    cpu.classList.add("computerText")
+
+    let gpu = position.appendChild(document.createElement("p"))  
+    gpu.textContent = `Graphics: ${computer.gpu}`
+    gpu.classList.add("computerText")
+  };
+  
+  
+  const populatePage = () => {
+    getInfo('0', productContainer1, product1);
+    getInfo('1', productContainer2, product2);
+    getInfo('2', productContainer3, product3);
+    getInfo('3', productContainer4, product4);
+  };
+  
+  populatePage ()
--- models/index.js
+++ models/index.js
@@ -2,13 +2,16 @@ const mongoose = require("mongoose");
 const productSchema = require("./product");
 const brandSchema = require("./brand");
 const sasuageSchema = require("./sausage")
+const reviewSchema = require("./review")
 
 const Product = mongoose.model("Product", productSchema);
 const Brand = mongoose.model("Brand", brandSchema);
 const Sausage = mongoose.model("Sausage", sasuageSchema)
+const Review = mongoose.model("Review", reviewSchema)
 
 module.exports = {
   Product,
   Brand,
-  Sausage
+  Sausage,
+  Review
 };
